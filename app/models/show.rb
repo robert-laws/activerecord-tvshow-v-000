4d7a4fb3aa2f05867a7b8bcadb113aa3661acf43
Robert Laws <roblaws@me.com>
2017-10-21 08:28:02 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     show = Show.find_by(rating: self.highest_rating)
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    show = Show.find_by(rating: self.lowest_rating)
+  end
 end
